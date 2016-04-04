86bebed26699cfbcbb0cad66cd6dc0b8f944bc19
Chad Johnson <chad.d.johnson@gmail.com>
2016-04-03 23:30:08 -0500

    Using oanda-adapter for streaming.

--- stream.js
+++ stream.js
@@ -2,105 +2,27 @@ var config = require('./config.json');
 var https = require('https');
 var colors = require('colors');
 var mongoose = require('mongoose');
+var OANDAAdapter = require('oanda-adapter');
 var Tick = require('./models/Tick');
 
-var reconnectDelay = 1000;
+var symbols = ['NZD_USD', 'AUD_JPY', 'USD_JPY', 'AUD_CAD', 'AUD_NZD', 'AUD_USD', 'CAD_JPY', 'EUR_GBP', 'EUR_USD', 'GBP_JPY', 'USD_CAD', 'USD_CHF', 'EUR_JPY', 'GBP_USD'];
+var client = new OANDAAdapter({
+    environment: 'live',
+    accessToken: config.accessToken
+});
+
+console.log('Starting client');
 
 mongoose.connect('mongodb://localhost/forex-data');
 mongoose.connection.on('error', console.error.bind(console, 'Database connection error:'));
 
-function stream() {
-    var requestOptions = {
-        host: config.host,
-        path: '/v1/prices?accountId=5280798&instruments=' + config.instruments.join('%2C'),
-        method: 'GET',
-        headers: {'Authorization' : 'Bearer ' + config.accessToken},
-    };
-    var request = https.request(requestOptions, function(response) {
-        var noDataTimeout = 0;
-        var bodyChunk = '';
-        var data = null;
-
-        console.log('Connected at ' + new Date());
-
-        // Reset reconnection delay.
-        reconnectDelay = 1000;
-
-        response.on('data', function(chunk) {
-            clearTimeout(noDataTimeout);    
-            noDataTimeout = setTimeout(function() {
-                console.log('No data received for 10 seconds; aborting'.red);
-                request.abort();
-            }, 10 * 1000);
-
-            try {
-                bodyChunk = chunk.toString().trim();
-
-                if (!bodyChunk) {
-                    // No data.
-                    return;
-                }
-
-                if (bodyChunk.indexOf('\n') > -1) {
-                    // Multiple ticks found.
-                    bodyChunk = bodyChunk.replace(/\n/g, ',');
-                }
-
-                // Use an array.
-                bodyChunk = '[' + bodyChunk + ']';
-
-                // Parse the data.
-                data = JSON.parse(bodyChunk);
-
-                data.forEach(function(item) {
-                    var tick = item.tick;
-                    var documentData = null;
-
-                    if (tick) {
-                        documentData = {
-                            symbol: tick.instrument.replace('_', ''),
-                            bid: tick.bid,
-                            ask: tick.ask,
-                            timestamp: new Date(tick.time)
-                        };
-                        Tick.create(documentData);
-                    }
-                });
-            }
-            catch (error) {
-                console.error(error.toString().red);
-            }
+symbols.forEach(function(symbol) {
+    client.subscribePrice(config.accountId, symbol, function(tick) {
+        Tick.create({
+            symbol: tick.instrument.replace('_', ''),
+            bid: tick.bid,
+            ask: tick.ask,
+            timestamp: tick.time
         });
-        response.on('end', function(chunk) {
-            clearTimeout(noDataTimeout);    
-
-            console.log('Disconnected at ' + new Date() + ': ' + response.statusCode);
-            console.log();
-
-            setTimeout(function() {
-                // Restart streaming.
-                stream();
-            }, reconnectDelay);
-
-            // Double reconnection delay timeout, per documentation.
-            reconnectDelay *= 2;
-        });
-    });
-    request.on('error', function(error) {
-        console.log(error.toString().red);
-
-        if (error.code !== 'ENOTFOUND') {
-            // Error is something other than no Internet connection.
-            // Double reconnection delay timeout, per documentation.
-            reconnectDelay *= 2;
-        }
-
-        setTimeout(function() {
-            // Restart streaming.
-            stream();
-        }, reconnectDelay);
-    });
-    request.end();
-}
-
-stream();
+    }, this);
+});
