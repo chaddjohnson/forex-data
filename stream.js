8d35b46588850fdf6cc87b938cab8f6c70428355
Chad Johnson <chad.d.johnson@gmail.com>
2016-08-23 01:30:12 -0500

    Using symbols from config.

--- stream.js
+++ stream.js
@@ -5,7 +5,6 @@ var mongoose = require('mongoose');
 var OANDAAdapter = require('oanda-adapter');
 var Tick = require('./models/Tick');
 
-var symbols = ['NZD_USD', 'AUD_JPY', 'USD_JPY', 'AUD_CAD', 'AUD_NZD', 'AUD_USD', 'CAD_JPY', 'EUR_GBP', 'EUR_USD', 'GBP_JPY', 'USD_CAD', 'USD_CHF', 'EUR_JPY', 'GBP_USD'];
 var client = new OANDAAdapter({
     environment: 'live',
     accessToken: config.accessToken
@@ -16,7 +15,7 @@ console.log('Starting client');
 mongoose.connect('mongodb://localhost/forex-data');
 mongoose.connection.on('error', console.error.bind(console, 'Database connection error:'));
 
-symbols.forEach(function(symbol) {
+config.instruments.forEach(function(symbol) {
     client.subscribePrice(config.accountId, symbol, function(tick) {
         Tick.create({
             symbol: tick.instrument.replace('_', ''),
